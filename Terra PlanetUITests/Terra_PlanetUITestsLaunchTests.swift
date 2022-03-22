9aeed71834015ed99fffcecbb4439d7fde5b123b
f0go <f0go.dev@gmail.com>
2022-03-22 10:38:17 -0300

    Initial Commit

---
+++ Terra PlanetUITests/Terra_PlanetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Terra_PlanetUITestsLaunchTests.swift
+//  Terra PlanetUITests
+//
+//  Created by f0go on 22/03/2022.
+//
+
+import XCTest
+
+class Terra_PlanetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
