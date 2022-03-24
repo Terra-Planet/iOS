7d3cfe553f61a71bc14b0679ba14f38944c71a2b
f0go <f0go.dev@gmail.com>
2022-03-24 15:20:39 -0300

    QR utils

---
+++ Terra Planet/Utils/QR.swift
@@ -0,0 +1,25 @@
+//
+//  QR.swift
+//  Terra Planet
+//
+//  Created by f0go on 24/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class QR {
+    class func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        if let QRFilter = CIFilter(name: "CIQRCodeGenerator") {
+            QRFilter.setValue(data, forKey: "inputMessage")
+            guard let QRImage = QRFilter.outputImage else {return nil}
+            
+            let transformScale = CGAffineTransform(scaleX: 10.0, y: 10.0)
+            let scaledQRImage = QRImage.transformed(by: transformScale)
+            
+            return UIImage(ciImage: scaledQRImage)
+        }
+        return nil
+    }
+}
