d895c4f012b078df4e4a1409723d67c24681deca
f0go <f0go.dev@gmail.com>
2022-03-24 15:21:49 -0300

    Util -> Rounded doubles

---
+++ Terra Planet/Utils/Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  Terra Planet
+//
+//  Created by f0go on 24/03/2022.
+//
+
+import Foundation
+
+extension Double {
+    func double2String() -> String {
+        return String(format: "%.2f", self)
+    }
+}
