3829ed7d811c8c329b9be9059e0d0fa74e80f273
f0go <f0go.dev@gmail.com>
2022-03-25 14:04:46 -0300

    swap

---
+++ Terra Planet/Views/Swap/Swap.swift
@@ -0,0 +1,68 @@
+//
+//  Swap.swift
+//  Terra Planet
+//
+//  Created by f0go on 24/03/2022.
+//
+
+import Foundation
+import UIKit
+
+extension SwapVC {
+
+    func calculateLiveSwap(textField: UITextField) {
+        if var text = textField.text {
+            text = text.replacingOccurrences(of: ",", with: ".")
+            textField.text = text
+            if let double = Double(text) {
+                if textField == fromAmount {
+                    if from == "uluna" {
+                        toAmount.text = "\((double * API.shared.lunaPrice).double2String())"
+                    }
+                    else {
+                        toAmount.text = "\((double / API.shared.lunaPrice))"
+                    }
+                }
+                else {
+                    if from == "uluna" {
+                        fromAmount.text = "\((double / API.shared.lunaPrice))"
+                    }
+                    else {
+                        fromAmount.text = "\((double * API.shared.lunaPrice).double2String())"
+                    }
+                }
+            }
+        }
+    }
+    
+    func flip() {
+        fromAmount.resignFirstResponder()
+        toAmount.resignFirstResponder()
+        fromAmount.text = "0"
+        toAmount.text = "0"
+        if from == "uluna" {
+            from = "uusd"
+            to = "uluna"
+            fromLogo.image = UIImage(named: "uusd")
+            toLogo.image = UIImage(named: "uluna")
+            fromCoinName.text = "UST"
+            toCoinName.text = "LUNA"
+        }
+        else {
+            from = "uluna"
+            to = "uusd"
+            fromLogo.image = UIImage(named: "uluna")
+            toLogo.image = UIImage(named: "uusd")
+            fromCoinName.text = "LUNA"
+            toCoinName.text = "UST"
+        }
+    }
+    
+    func swap(callback: @escaping (_ status: Bool) -> Void) {
+        if let amount = fromAmount.text {
+            API.shared.swap(from: from, to: to, amount: amount) { status in
+                callback(status)
+            }
+        }
+    }
+}
---
+++ Terra Planet/Views/Swap/SwapVC.swift
@@ -0,0 +1,59 @@
+//
+//  SwapVC.swift
+//  Terra Planet
+//
+//  Created by f0go on 24/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class SwapVC: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var fromView: UIView!
+    @IBOutlet weak var toView: UIView!
+    @IBOutlet weak var fromLogo: UIImageView!
+    @IBOutlet weak var fromCoinName: UILabel!
+    @IBOutlet weak var toLogo: UIImageView!
+    @IBOutlet weak var toCoinName: UILabel!
+    @IBOutlet weak var fromAmount: UITextField!
+    @IBOutlet weak var toAmount: UITextField!
+    
+    var from = "uluna"
+    var to = "uusd"
+    
+    override func viewDidLoad() {
+        design()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "0" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        calculateLiveSwap(textField: textField)
+    }
+    
+    @IBAction func switchCoins(_ sender: UIButton) {
+        flip()
+    }
+    
+    @IBAction func swap(_ sender: UIButton) {
+        let loading = UIAlertController(title: "Loading", message: "Please wait...", preferredStyle: .alert)
+        present(loading, animated: true)
+        swap { status in
+            Utils.shared.events.trigger("reloadBalance")
+            loading.dismiss(animated: true)
+            self.dismiss(animated: true)
+        }
+    }
+    
+    func design() {
+        fromView.layer.borderWidth = 2
+        fromView.layer.borderColor = UIColor(named: "color_blue")?.cgColor
+        toView.layer.borderWidth = 2
+        toView.layer.borderColor = UIColor(named: "color_blue")?.cgColor
+    }
+}
