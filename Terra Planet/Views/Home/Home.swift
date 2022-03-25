daa8c0746c3b2ae40cb9575cf19a142a8d856d82
f0go <f0go.dev@gmail.com>
2022-03-25 14:04:20 -0300

    home

---
+++ Terra Planet/Views/Home/Home.swift
@@ -0,0 +1,45 @@
+//
+//  Home.swift
+//  Terra Planet
+//
+//  Created by f0go on 23/03/2022.
+//
+
+import Foundation
+import UIKit
+
+extension HomeVC {
+    func loadBalance() {
+        DispatchQueue.main.async {
+            let loading = UIAlertController(title: "Loading", message: "Please wait...", preferredStyle: .alert)
+            self.present(loading, animated: true)
+            API.shared.loadCoins(reloadMarket: true) { status in
+                loading.dismiss(animated: true)
+                if status, let wallet = API.shared.wallet {
+                    self.balance.text = "$\(wallet.balance().double2String())"
+                    for item in wallet.coins {
+                        switch item.value.coin {
+                        case "uluna":
+                            self.lunaAmount.text = "\(item.value.amount)"
+                            self.lunaValue.text = "$\((item.value.amount * API.shared.lunaPrice).double2String())"
+                        case "uusd":
+                            self.ustAmount.text = "\(item.value.amount)"
+                            self.ustValue.text = "$\(item.value.amount.double2String())"
+                        case "anchor":
+                            self.anchorValue.text = "$\(item.value.amount.double2String())"
+                        default:
+                            print(item.value.coin)
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    
+    func loadAPY() {
+        API.shared.apy { apy in
+            self.anchorAPY.text = "Earn (\(apy.double2String())% APY)"
+        }
+    }
+}
---
+++ Terra Planet/Views/Home/HomeVC.swift
@@ -0,0 +1,68 @@
+//
+//  HomeVC.swift
+//  Terra Planet
+//
+//  Created by f0go on 23/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    @IBOutlet weak var actionsView: UIView!
+    @IBOutlet weak var earnView: UIView!
+    
+    @IBOutlet weak var balance: UILabel!
+    
+    @IBOutlet weak var lunaAmount: UILabel!
+    @IBOutlet weak var lunaValue: UILabel!
+    
+    
+    @IBOutlet weak var ustAmount: UILabel!
+    @IBOutlet weak var ustValue: UILabel!
+    
+    @IBOutlet weak var anchorAPY: UILabel!
+    @IBOutlet weak var anchorValue: UILabel!
+    
+    override func viewDidLoad() {
+        design()
+        loadBalance()
+        loadAPY()
+        Utils.shared.events.listenTo("reloadBalance") { _ in
+            self.loadBalance()
+        }
+    }
+    
+    @IBAction func receive(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Receive", bundle: nil).instantiateViewController(identifier: "ReceiveVC")
+        self.present(vc, animated: true)
+    }
+    
+    @IBAction func swap(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Swap", bundle: nil).instantiateViewController(identifier: "SwapVC")
+        self.present(vc, animated: true)
+    }
+    
+    @IBAction func send(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Send", bundle: nil).instantiateViewController(identifier: "SendVC")
+        self.present(vc, animated: true)
+    }
+    
+    @IBAction func anchorDeposit(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Anchor", bundle: nil).instantiateViewController(identifier: "AnchorVC") as! AnchorVC
+        vc.anchorAction = .deposit
+        self.present(vc, animated: true)
+    }
+    
+    @IBAction func anchorWithdraw(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Anchor", bundle: nil).instantiateViewController(identifier: "AnchorVC") as! AnchorVC
+        vc.anchorAction = .withdraw
+        self.present(vc, animated: true)
+    }
+    
+    private func design() {
+        earnView.layer.borderWidth = 2
+        earnView.layer.borderColor = UIColor(named: "color_blue")?.cgColor
+    }
+}
